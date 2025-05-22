274b0dcb885d9ebba15d241e92eee482ffbc6846
bequ1n <kulikovskikh@bk.ru>
2025-05-22 03:02:45 +0300

    add block C1

--- lab_1C/RootsSeparation.m
+++ lab_1C/RootsSeparation.m
@@ -1,12 +1,10 @@
-
-
 function [A, B] = RootsSeparation(f, a, b, N, Hmin, PlotFlag)
   temp = a;
   A = [];
   B = [];
   while (N > 0) && (temp < b)
     if sign(f(temp)) ~= sign(f(temp + Hmin))
-      A = [A, temp];
+      A = [A, temp-Hmin];
       B = [B, temp+Hmin];
       N = N - 1;
     endif
@@ -19,22 +17,11 @@ function [A, B] = RootsSeparation(f, a, b, N, Hmin, PlotFlag)
     grid on;
     x = linspace(a, b, 1000);
     plot(x, f(x), 'b', 'LineWidth', 2);
-    plot(A, zeros(size(A)), 'ro', 'MarkerSize', 8, 'LineWidth', 1.5);
-    plot(B, zeros(size(B)), 'ro', 'MarkerSize', 8, 'LineWidth', 1.5);
+    plot(A, zeros(size(A)), 'ro', 'MarkerSize', 2, 'LineWidth', 1.5);
+    plot(B, zeros(size(B)), 'ro', 'MarkerSize', 2, 'LineWidth', 1.5);
     xlabel('x');
     ylabel('f(x)');
     title('Интервалы с корнями (отмечены красным)');
     legend('Функция', 'Границы интервалов', 'Location', 'northwest');
   endif
 endfunction
-%}
-
-k = poly([-0.2, -6, 6, -8, -1, -5, 7, 8]);
-f = @(x) polyval(k, x);
-
-a = -10;
-b = 10;
-h_min = (b-a)/1e6;
-[A, B] = RootsSeparation(f, -10, 10, 8, h_min, 0);
-A
-B
---
+++ lab_1C/main.m
@@ -0,0 +1,11 @@
+
+k = poly([-0.2, -6, 6, -8, -1, -5, 7, 8]);
+f = @(x) polyval(k, x);
+
+a = -10;
+b = 10;
+h_min = (b-a)/10000;
+[A, B] = RootsSeparation(f, -8.1, 8.1, 8, h_min, 1);
+A
+B
+waitfor(gcf);
