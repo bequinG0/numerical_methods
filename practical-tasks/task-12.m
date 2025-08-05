43b268fb1be531e31550b7e98395d52d0afbc3be
bequ1nG0 <kulikovskikh@bk.ru>
2025-08-05 13:56:35 +0300

    complete tasks for nonlinear equations

--- practical-tasks/task-12.m
+++ practical-tasks/task-12.m
@@ -1,12 +1,73 @@
+warning('off', 'Octave:missing-FcInit');
+
 % численнное решение уравнения x - sin(x) = 0.25
 
-f = @(x) x - sin(x) - 0.25
+fprintf("[*] численнное решение уравнения x - sin(x) = 0.25\n");
 
-[L, R] = RootsSeparation(f, 0, 2, 100, 0.01, 0)
+f = @(x) x - sin(x) - 0.25;
 
+[left, right] = RootsSeparation(f, 0, 2, 100, 0.01, 1)
 
+roots_1 = [];
+roots_2 = [];
+roots_3 = [];
 
+for i = 1:length(left)
+    [roots_1, ~] = edit_bisection(f, left(i), right(i), 1e-3, 0);
+    [roots_2, ~] = newton_method(f, left(i), right(i), 1e-3, 0);
+    [roots_3, ~] = secant_method(f, left(i), right(i), 1e-3, 0);
+end
+
+roots_1 
+roots_2 
+roots_3
+
+waitfor(gcf);
 
 % численное решение уравнения e^x - x^2 = 0
 
+fprintf("[*] численнное решение уравнения e^x - x^2 = 0\n");
+f = @(x) exp(x) - x.^2;
+
+[left, right] = RootsSeparation(f, -1, 0, 100, 0.01, 1)
+
+roots_1 = [];
+roots_2 = [];
+roots_3 = [];
+
+for i = 1:length(left)
+    [roots_1, ~] = edit_bisection(f, left(i), right(i), 1e-3, 0);
+    [roots_2, ~] = newton_method(f, left(i), right(i), 1e-3, 0);
+    [roots_3, ~] = secant_method(f, left(i), right(i), 1e-3, 0);
+end
+
+roots_1 
+roots_2 
+roots_3
+
+waitfor(gcf);
+
 % численное решение уравнения x^3 + 3x^2 - 24x + 1 = 0
+
+fprintf("[*] численнное решение уравнения x^3 + 3x^2 - 24x + 1 = 0\n");
+f = @(x) x.^3 + 3*x.^2 - 24*x + 1;
+
+[left, right] = RootsSeparation(f, -7, 5, 100, 0.01, 1)
+
+roots_1 = [];
+roots_2 = [];
+roots_3 = [];
+
+for i = 1:length(left)
+    [roots_1(end+1), ~] = edit_bisection(f, left(i), right(i), 1e-3, 0);
+    [roots_2(end+1), ~] = newton_method(f, left(i), right(i), 1e-3, 0);
+    [roots_3(end+1), ~] = secant_method(f, left(i), right(i), 1e-3, 0);
+end
+
+roots_1 
+roots_2 
+roots_3
+
+waitfor(gcf);
+
+
