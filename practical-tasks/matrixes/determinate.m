03962582e429b9069747785b5bb53998c4085c13
bequ1nG0 <kulikovskikh@bk.ru>
2025-08-11 00:47:07 +0300

    complete task 2 from matrixes

--- practical-tasks/matrixes/determinate.m
+++ practical-tasks/matrixes/determinate.m
@@ -1,11 +1,13 @@
 function one = determinate(A, n)
     if n == 1
-        return A(1, 1);
+        one = A(1, 1);
+        return;
     endif
     if n > 1
-        ans = 0;
+        res = 0;
         for i = 1:n 
-            ans = ans + (-1)^(i+1) * A(1, i) * determinate(separeted(A, 1, i, n-1), n-1);
+            res = res + (-1)^(i+1) * A(1, i) * determinate(separeted(A, 1, i, n), n-1);
         endfor
+        one = res;
     endif
 endfunction
--- practical-tasks/matrixes/separeted.m
+++ practical-tasks/matrixes/separeted.m
@@ -1,13 +1,16 @@
 function TEMP = separeted(A, a, b, n)
     TEMP = zeros(n-1, n-1);
-    for i = 1:n 
-        if i ~= a 
-            for j = 1:n 
+    row = 1;
+    for i = 1:n
+        if i ~= a
+            col = 1;
+            for j = 1:n
                 if j ~= b
-                    TEMP = A(i, j);
-                endif 
-            endfor
-        endif
-    endfor
-    return TEMP;
-endfunction
+                    TEMP(row, col) = A(i, j);
+                    col = col + 1;
+                end
+            end
+            row = row + 1;
+        end
+    end
+end
